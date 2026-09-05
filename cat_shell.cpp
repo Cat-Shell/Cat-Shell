98eca80e6c67881ae6132a1804ddffea586fde93
FelineFantasy <mailsalavata5@gmail.com>
2026-09-05 23:44:21 +0500

    refactor: remove redundant std:: qualification with using namespace std

--- cat_shell.cpp
+++ cat_shell.cpp
@@ -9,21 +9,21 @@ using namespace std;
 
 // ! Функция для вывода списка файлов и папок в текущей директории
 void ls() {
-    // ! Используем std::filesystem для итерации по текущей директории
-    for (const auto &entry : std::filesystem::directory_iterator(std::filesystem::current_path())) {
+    // ! Используем filesystem для итерации по текущей директории
+    for (const auto &entry : filesystem::directory_iterator(filesystem::current_path())) {
         // ! Выводим имя файла или папки
         cout << entry.path().filename().string() << "\n";
     }
 }
 
 // ! Функция для получения текущего пути и формирования приглашения
-std::string get_prompt() {
-    return "🐱 " + std::filesystem::current_path().string() + " > ";
+string get_prompt() {
+    return "🐱 " + filesystem::current_path().string() + " > ";
 }
 
 // ! Функция для печати приветственного сообщения
 void print_welcome() {
-    std::cout << R"(
+    cout << R"(
   /\_/\
  ( o.o )
   > ^ <   Cat-Shell v0.2.1
@@ -33,12 +33,12 @@ void print_welcome() {
 
 // ! Функция для печати справки по командам
 void print_help() {
-    std::cout << "help - эта справка\n";
-    std::cout << "clear - очистить экран\n";
-    std::cout << "meow - мяукнуть\n";
-    std::cout << "pwd - текущий путь\n";
-    std::cout << "ls - список файлов и папок\n";
-    std::cout << "exit - выйти\n";
+    cout << "help - эта справка\n";
+    cout << "clear - очистить экран\n";
+    cout << "meow - мяукнуть\n";
+    cout << "pwd - текущий путь\n";
+    cout << "ls - список файлов и папок\n";
+    cout << "exit - выйти\n";
 }
 
 int main() {
@@ -49,14 +49,14 @@ int main() {
     // ! Печатаем приветственное сообщение
     print_welcome();
 
-    std::string input;
+    string input;
 
     while (true) {
         // ! Выводим приглашение с текущим путём
-        std::cout << get_prompt();
+        cout << get_prompt();
 
         // ! Получаем команду пользователя
-        std::getline(std::cin, input);
+        getline(cin, input);
 
         // ! Проверяем команду выхода из Cat-Shell
         if (input == "exit") {
@@ -70,17 +70,17 @@ int main() {
 
         // ! Обрабатываем команду meow для вывода мяуканья
         else if (input == "meow") {
-            std::cout << "Мяу!\n";
+            cout << "Мяу!\n";
         }
 
         // ! Обрабатываем команду clear для очистки экрана
         else if (input == "clear") {
-            std::cout << "\033[H\033[J";
+            cout << "\033[H\033[J";
         }
 
         // ! Обрабатываем команду pwd для вывода текущего пути
         else if (input == "pwd") {
-            std::cout << std::filesystem::current_path() << "\n";
+            cout << filesystem::current_path() << "\n";
         }
 
         // !Обрабатываем команду ls для вывода списка файлов и папок
@@ -90,10 +90,11 @@ int main() {
 
         // ! Проверяем неизвестные команды
         else {
-            std::cout << "Команда не найдена. Напиши help.\n";
+            cout << "Команда не найдена. Напиши help.\n";
         }
 
     }
 
     return 0;
 }
+
