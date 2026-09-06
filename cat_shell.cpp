be1a677edfedf6eef09382b1044a5d19571a5668
FelineFantasy <mailsalavata5@gmail.com>
2026-09-06 23:01:09 +0500

    fix: fix Linux build by isolating Windows.h

--- cat_shell.cpp
+++ cat_shell.cpp
@@ -2,26 +2,27 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-#include <Windows.h>
 
-// ! пространство имен std
+// Включаем Windows.h только если компилируем под Windows
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
 using namespace std;
 
-// ! Функция для вывода списка файлов и папок в текущей директории
+// Функция для вывода списка файлов и папок в текущей директории
 void ls() {
-    // ! Используем filesystem для итерации по текущей директории
     for (const auto &entry : filesystem::directory_iterator(filesystem::current_path())) {
-        // ! Выводим имя файла или папки
         cout << entry.path().filename().string() << "\n";
     }
 }
 
-// ! Функция для получения текущего пути и формирования приглашения
+// Функция для получения текущего пути и формирования приглашения
 string get_prompt() {
     return "🐱 " + filesystem::current_path().string() + " > ";
 }
 
-// ! Функция для печати приветственного сообщения
+// Функция для печати приветственного сообщения
 void print_welcome() {
     cout << R"(
   /\_/\
@@ -31,7 +32,7 @@ void print_welcome() {
 )";
 }
 
-// ! Функция для печати справки по командам
+// Функция для печати справки по командам
 void print_help() {
     cout << "help - эта справка\n";
     cout << "clear - очистить экран\n";
@@ -42,59 +43,42 @@ void print_help() {
 }
 
 int main() {
-
+    // Настройка кодировки для Windows
+#ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
     setlocale(LC_ALL, ".UTF8");
+#endif
 
-    // ! Печатаем приветственное сообщение
     print_welcome();
 
     string input;
 
     while (true) {
-        // ! Выводим приглашение с текущим путём
         cout << get_prompt();
-
-        // ! Получаем команду пользователя
         getline(cin, input);
 
-        // ! Проверяем команду выхода из Cat-Shell
         if (input == "exit") {
             break;
         }
-
-        // ! Обрабатываем команду help для вывода справки
         else if (input == "help") {
             print_help();
         }
-
-        // ! Обрабатываем команду meow для вывода мяуканья
         else if (input == "meow") {
             cout << "Мяу!\n";
         }
-
-        // ! Обрабатываем команду clear для очистки экрана
         else if (input == "clear") {
             cout << "\033[H\033[J";
         }
-
-        // ! Обрабатываем команду pwd для вывода текущего пути
         else if (input == "pwd") {
-            cout << filesystem::current_path() << "\n";
+            cout << filesystem::current_path().string() << "\n";
         }
-
-        // !Обрабатываем команду ls для вывода списка файлов и папок
         else if (input == "ls") {
-                ls();
+            ls();
         }
-
-        // ! Проверяем неизвестные команды
-        else {
-            cout << "Команда не найдена. Напиши help.\n";
+        else if (!input.empty()) {
+            cout << "Cat-Shell: команда не найдена: " << input << "\n";
         }
-
     }
 
     return 0;
 }
-
