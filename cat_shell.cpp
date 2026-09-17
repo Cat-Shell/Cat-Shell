543a3fea687ae42d7672fa2852351191c60a3143
SecDetxcoding <secdetxcoding@gmail.com>
2026-09-17 21:08:31 +0600

    added usability to the cd command

--- cat_shell.cpp
+++ cat_shell.cpp
@@ -205,6 +205,8 @@ bool execute_command(const string &input) {
     } else if (command == "kitty") {
         cmd_kitty(argument);
 
+    } else if (command == "cd") {
+        cmd_cd(argument);
     } else {
         cout << "Cat-Shell: команда не найдена: " << command << "\n";
         cout << "Котик не нашел ее" << endl;
