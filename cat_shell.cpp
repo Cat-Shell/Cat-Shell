23c46f30457f6a5c1805837ab33aeba4e65b515f
FelineFantasy <mailsalavata5@gmail.com>
2026-09-24 23:47:42 +0500

    fix: command number output in history

--- cat_shell.cpp
+++ cat_shell.cpp
@@ -207,7 +207,7 @@ void cmd_history(const vector<string>& history) {
     }
 
     for (size_t i = 0; i < history.size(); i++) {
-        cout << 1 + 1 << " " << history[i] << endl;
+        cout << (i + 1) << " " << history[i] << endl;
     }
 
 }
